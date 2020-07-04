2e31a0b587a16b92ea1fdc01625b0b363442df3a
DiegoRutolo <diegorutolo@gmail.com>
2020-07-04 22:51:49 +0200

    Lógica interna del movimiento

--- pilla.py
+++ pilla.py
@@ -6,19 +6,25 @@ from src import componentes
 #region Constantes
 SCREEN_TITULO = "Pilla-Pilla"
 
-#Tamaño de los cuadros
-TCEL = 64
-N_COLS = 12
-N_FILS = 10
+TCEL = 64	#Tamaño de los cuadros
+N_COLS = 18
+N_FILS = 12
+
+MARGEN_TOP = 10
 
 SCREEN_ANCHO = TCEL * N_COLS
-SCREEN_ALTO = TCEL * N_FILS
+SCREEN_ALTO = TCEL * N_FILS + MARGEN_TOP
 
-RES_PATH = "res/img/terreno/"
-IMG_PATH_TERRENO = [
-	RES_PATH + "mapTile_022.png",
-	RES_PATH + "mapTile_027.png"
+PATH_RES = "res/img/"
+PATH_RES_TERRENO = PATH_RES + "terreno/"
+PATH_IMG_TERRENO = [
+	PATH_RES_TERRENO + "mapTile_022.png",
+	PATH_RES_TERRENO + "mapTile_027.png"
 ]
+PATH_IMG_J1 = PATH_RES + "pieceYellow_multi05.png"
+PATH_IMG_J2 = PATH_RES + "piecePurple_border01.png"
+COLOR_J1 = arcade.csscolor.YELLOW
+COLOR_J2 = arcade.csscolor.PURPLE
 #endregion
 
 class Pilla(arcade.Window):
@@ -28,7 +34,11 @@ class Pilla(arcade.Window):
 		arcade.set_background_color(arcade.csscolor.LAVENDER)
 
 		self.sprites_tablero = None
+		self.sprites_actores = None
 		self.tablero = None
+		self.j1 = None
+		self.j2 = None
+		self.color_perseguidor = None
 
 	def setup(self):
 		#region Generar terreno
@@ -38,10 +48,10 @@ class Pilla(arcade.Window):
 
 		# Renderizar el tablero
 		self.sprites_tablero = arcade.SpriteList()
-		for col in self.tablero.world:
+		for col in self.tablero.casillas:
 			for casilla in col:
 				# Crear el sprite con el nivel que le corresponde
-				sprite = arcade.Sprite(IMG_PATH_TERRENO[casilla.nivel])
+				sprite = arcade.Sprite(PATH_IMG_TERRENO[casilla.nivel])
 				# Calcular la posición según sus coordenadas
 				sprite.center_x = (TCEL / 2) + (TCEL * casilla.x)
 				sprite.center_y = (TCEL / 2) + (TCEL * casilla.y)
@@ -52,11 +62,35 @@ class Pilla(arcade.Window):
 		#endregion
 
 		#region Colocar jugadores
+
+		# Crear sprites actores
+		
 		#endregion
+
+		# Establecer el color del perseguidor inicial
+		self.color_perseguidor = arcade.csscolor.YELLOW
 	
 	def on_draw(self):
 		arcade.start_render()
+
+		# Indicador de perseguidor
+		indicador = arcade.SpriteSolidColor(SCREEN_ANCHO, MARGEN_TOP, self.color_perseguidor)
+		indicador.top = SCREEN_ALTO
+		indicador.left = 0
+		indicador.draw()
+
 		self.sprites_tablero.draw()
+		#self.sprites_actores.draw()
+	
+	def get_coords_from_pos(self, x, y):
+		return ((TCEL / 2) + (TCEL * x), (TCEL / 2) + (TCEL * y))
+	
+	def on_key_press(self, symbol: int, modifiers: int):
+		if symbol == arcade.key.C:
+			if self.color_perseguidor == COLOR_J1:
+				self.color_perseguidor = COLOR_J2
+			else:
+				self.color_perseguidor = COLOR_J1
 
 #end Pilla
 
--- src/componentes.py
+++ src/componentes.py
@@ -1,31 +1,30 @@
-import arcade
 import random
+import enum
 
 #region Constantes
+DENSIDAD_ALTILLOS = 0.3
 #endregion
+
+class Direccion(enum.Enum):
+	NORTE = 'n'
+	SUR = 's'
+	ESTE = 'e'
+	OESTE = 'o'
 
 class Casilla():
 	"""Objeto que representa cada casilla
 
 	Atributos:
 		nivel: indica la altura
-		abierta: inida si un actor se puede mover
+		transitable: inida si un actor se puede mover a esta casilla
 	"""
 
-	def __init__(self, x, y, nivel=0, abierta=True):
+	def __init__(self, x, y, nivel=0, transitable=True):
 		self.nivel = nivel
-		self.abierta = abierta
+		self.abierta = transitable
 		self.x = x
 		self.y = y
 
-
-class Actor():
-	"""Objetos que se puede mover e interactuar con el entorno
-	"""
-
-	def __init__(self, world, casilla_inicial: Casilla):
-		self.world = world
-		
 
 class Tablero():
 	"""Objeto que representa el tablero
@@ -42,9 +41,77 @@ class Tablero():
 		self.ncols = ncols
 		self.nfils = nfils
 		self.wrap = wrap
-		# Generar las casillas
-		self.world = [[Casilla(x, y) for y in range(nfils)] for x in range(ncols)]
 
-		# Hacer una fila de casillas de nivel 1
+		# Generar las casillas
+		self.casillas = [[Casilla(x, y) for y in range(nfils)] for x in range(ncols)]
+
+		# Elevar algunas casillas
 		for x in range(ncols):
-			self.world[x][3].nivel = 1
+			for y in range(nfils):
+				if random.random() < DENSIDAD_ALTILLOS:
+					self.casillas[x][y].nivel = 1
+
+
+class Actor():
+	"""Objetos que se puede mover e interactuar con el entorno
+	"""
+
+	def __init__(self, world, x0, y0):
+		self.world = world
+		self.x = x0
+		self.y = y0
+	
+	def mover(self, dir: Direccion, n_casillas=1):
+		# Paso a paso
+		if n_casillas > 1:
+			self.mover(dict, n_casillas-1)
+
+		# Guardamos la casilla objetivo
+		obj = None
+		if dir == Direccion.NORTE:
+			try:
+				obj = self.world.casillas[self.x][self.y + n_casillas]
+			except:
+				if self.world.wrap:
+					obj = self.world.casillas[self.x][0]
+				else:
+					return
+		if dir == Direccion.SUR:
+			try:
+				obj = self.world.casillas[self.x][self.y - n_casillas]
+			except:
+				if self.world.wrap:
+					obj = self.world.casillas[self.x][self.world.nfils]
+				else:
+					return
+		if dir == Direccion.ESTE:
+			try:
+				obj = self.world.casillas[self.x + n_casillas][self.y]
+			except:
+				if self.world.wrap:
+					obj = self.world.casillas[0][self.y]
+				else:
+					return
+		if dir == Direccion.OESTE:
+			try:
+				obj = self.world.casillas[self.x - n_casillas][self.y]
+			except:
+				if self.world.wrap:
+					obj = self.world.casillas[self.world.ncols][self.y]
+				else:
+					return
+		
+		# Comprobamos si es transitable
+		if not obj.transitable:
+			return
+		
+		# Comprobamos si está por encima
+		if self.world.casillas[self.x][self.y].nivel < obj.nivel:
+			return
+		
+		# Movemos
+		self.x = obj.x
+		self.y = obj.y
+
+	def get_casilla(self):
+		return self.world.casillas[self.x][self.y]
