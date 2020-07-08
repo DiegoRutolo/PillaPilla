4694624217bc675399ff987150524939a63d22b4
DiegoRutolo <diegorutolo@gmail.com>
2020-07-08 21:31:23 +0200

    Generador de terreno de bloques irregulares

--- src/componentes.py
+++ src/componentes.py
@@ -2,7 +2,7 @@ import random
 import enum
 
 #region Constantes
-DENSIDAD_ALTILLOS = -0.3
+DENSIDAD_ALTILLOS = 0.4
 SEED = random.randint(0, 2**32 - 1)
 #endregion
 
@@ -53,7 +53,7 @@ class Tablero():
 		# Elevar algunas casillas
 
 		generador = Generador(self.ncols, self.nfils, seed=SEED)
-		generador.bsp()
+		generador.bloques(n_bloques=5)
 		for x in range(ncols):
 			for y in range(nfils):
 				self.casillas[x][y].nivel = generador.get(x, y)
@@ -153,10 +153,38 @@ class Generador():
 		"""
 		return self.mapa[x][y]
 	
+	def bloques(self, n_bloques = 3, tamanho_min=3, tamanho_max=8):
+		for j in range(n_bloques):
+			# Punto aleatorio
+			x0 = random.randint(0, self.ncols-1)
+			y0 = random.randint(0, self.nfils-1)
+			
+			# Primer bloque
+			self.mapa[x0][y0] = 1
+			# Siguientes bloques
+			tamanho = random.randint(tamanho_min, tamanho_max)
+			bloques_colocados = 0
+			while bloques_colocados < tamanho:
+				# Dirección
+				dx = random.randint(-1, 1)
+				dy = random.randint(-1, 1)
+				try:
+					# Reposicionar el origen
+					if self.mapa[x0+dx][y0+dy] == 1:
+						x0, y0 = x0+dx, y0+dy
+						continue
+
+					self.mapa[x0+dx][y0+dy] = 1
+					bloques_colocados += 1
+				except:
+					continue
+	
 	def bsp(self):
 		""" Divide el mapa en habitaciones usando un árbol BSP
 		"""
 		self._partir_area((0, 0, self.ncols-1, self.nfils-1))
+		for h in self.lista_habitaciones:
+			self._crear_pasillo(h)
 	
 	def _partir_area(self, coords):
 		print("Generando " + str(coords))
@@ -202,4 +230,25 @@ class Generador():
 		
 		self._partir_area(self.lista_habitaciones[-1])
 		self._partir_area(self.lista_habitaciones[-2])
+
+	def _rnd_point_in_habitacion(self, habitacion):
+		return (random.randint(habitacion[0], habitacion[2]), random.randint(habitacion[1], habitacion[3]))
+
+	def _crear_pasillo(self, habitacion, n_pasillos=2):
+		n_pasillos_creados = 0
+		while n_pasillos_creados >= n_pasillos:
+			pass
+			# Elegir dirección y origren
+			#dir = random.choice(list(Direccion))
+			dir = Direccion.NORTE
+			x0, y0 = self._rnd_point_in_habitacion(habitacion)
 			
+			# Comprobar si hay otra habitación en esa dirección
+			flag_luz_al_final_del_tunel = False
+			if dir == Direccion.NORTE:
+				for y in range(habitacion[3], self.nfils):
+					if self.mapa[x0][y] == 0:
+						flag_luz_al_final_del_tunel = True
+						print(habitacion)
+			
+			# Hacer el pasillo
